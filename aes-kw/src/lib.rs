97a92bd503d686aaa367616789d74179b50e352b
Tony Arcieri <bascule@gmail.com>
2022-04-20 14:20:58 -0600

    aes-kw v0.2.1

--- aes-kw/src/lib.rs
+++ aes-kw/src/lib.rs
@@ -2,8 +2,7 @@
 #![doc = include_str!("../README.md")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg",
-    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg",
-    html_root_url = "https://docs.rs/aes-kw/0.2.0"
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg"
 )]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![forbid(unsafe_code)]
